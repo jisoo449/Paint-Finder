0db0d1cadf86c6f866fc88fbf50a02bb90efa995
Bak Jisoo <48276691+jisoo449@users.noreply.github.com>
2022-09-04 14:35:26 +0900

    .

---
+++ src/test/java/com/jijis/paint_finder/PaintFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jijis.paint_finder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaintFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
