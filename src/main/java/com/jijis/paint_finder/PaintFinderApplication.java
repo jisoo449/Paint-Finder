ff1a9aa73915b4db937143d94be8ba8f6d5a3e82
Bak Jisoo <48276691+jisoo449@users.noreply.github.com>
2022-09-30 17:59:33 +0900

    div 크기 맞추는거 해야 함

--- src/main/java/com/jijis/paint_finder/PaintFinderApplication.java
+++ src/main/java/com/jijis/paint_finder/PaintFinderApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PaintFinderApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PaintFinderApplication.class, args);
+		SpringApplication.run(PaintFinderApplication.class, args); 
 	}
 
 }
