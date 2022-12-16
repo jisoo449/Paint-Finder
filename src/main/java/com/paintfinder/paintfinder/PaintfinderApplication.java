10a7d369dc199a51e5f06bfdf43441159faced92
Bak Jisoo <48276691+jisoo449@users.noreply.github.com>
2022-12-16 16:18:14 +0900

    init

---
+++ src/main/java/com/paintfinder/paintfinder/PaintfinderApplication.java
@@ -0,0 +1,13 @@
+package com.paintfinder.paintfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaintfinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaintfinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paintfinder/paintfinder/PaintfinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paintfinder.paintfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaintfinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
